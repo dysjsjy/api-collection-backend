b234002b0f8471684d69a970958c5bb1fcd78ae6
dysjsjy <98462388+dysjsjy@users.noreply.github.com>
2024-12-11 22:36:42 +0800

    Feat/exception (#3)
    * config init
    
    * exception init

--- src/main/java/com/dysjsjy/common/ResultUtils.java
+++ src/main/java/com/dysjsjy/common/ResultUtils.java
@@ -1,6 +1,6 @@
 package com.dysjsjy.common;
 
-public class RequestUtils {
+public class ResultUtils {
 
     public static <T> BaseResponse<T> success(T data) {
         return new BaseResponse<>(0, data, "ok");
